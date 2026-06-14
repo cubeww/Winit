3addba5a9878d6cc3c16069072df54e4a01ead0a
cubeww <35161393+cubeww@users.noreply.github.com>
2026-06-15 01:00:41 +0800

    Align Win32 backend file layout

--- Winit.Win32/Util.cs
+++ Winit.Win32/Util.cs
@@ -1,3 +1,8 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Winit.Core;
+
 namespace Winit.Win32;
 
 internal static class Util
@@ -30,5 +35,30 @@ internal static class Util
     public static nint MakeLParam(int low, int high)
     {
         return unchecked((nint)((low & 0xffff) | ((high & 0xffff) << 16)));
+    }
+}
+
+internal static class Win32Error
+{
+    public static OsRequestException Request(
+        [CallerFilePath] string file = "",
+        [CallerLineNumber] int line = 0)
+    {
+        return new OsRequestException(Os(file, line));
+    }
+
+    public static EventLoopOsException EventLoop(
+        [CallerFilePath] string file = "",
+        [CallerLineNumber] int line = 0)
+    {
+        return new EventLoopOsException(Os(file, line));
+    }
+
+    public static OsException Os(
+        [CallerFilePath] string file = "",
+        [CallerLineNumber] int line = 0)
+    {
+        int error = Marshal.GetLastPInvokeError();
+        return new OsException(file, line, new Win32Exception(error));
     }
 }
